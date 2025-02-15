840d899044b75285175dbcbbac556d86e36ba10d
TechnoDev <74283450+TechnoX0@users.noreply.github.com>
2025-02-15 11:52:31 +0800

    changed server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://tic-tac-toe-server-1ihj.onrender.com");
 
 const App = () => {
     const canvasRef = useRef(null);
